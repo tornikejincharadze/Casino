76ff16fa21b63a934d8af1a45e9b7a427c1531da
Tornike jincharadze <tornikejincharadze1997@gmail.com>
2018-09-06 15:02:11 +0400

    full page

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watchSass = require("gulp-watch-sass");
+var minifyCSS = require('gulp-csso');
+var rename = require('gulp-rename');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(webserver({
+      fallback: 'index.html',
+      livereload: true,
+      open: true
+    }));
+});
+ 
+gulp.task('sass', function () {
+  return gulp.src('./app/css/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCSS())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task("sass:watch", () => {
+    gulp.watch([
+      "./app/css/**/*.scss"
+    ], ["sass"]);
+  });
+
+gulp.task('default', [ 'sass', 'webserver', 'sass:watch' ]);
